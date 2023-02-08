95c042b3413d1f281426660bd2a823fccabda2c1
rkvk <rukavichkina@tut.by>
2023-02-08 15:10:33 +0300

    41

---
+++ 41ex/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы, через запятую: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(","), int.Parse);
+int count = array.Count(x => x > 0);
+Console.WriteLine($"Колличество элементов больше 0: {count}"); 
